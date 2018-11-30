df2f01b14b93d37caeaad17301f073505a674fb6
pinizsi2magdolna <pinizsi2magdolna@ext.t-systems.hu>
2018-11-30 21:42:34 +0000

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,5 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {async, TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -10,22 +10,7 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
 
-  it(`should have as title 'messageboard'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('messageboard');
-  });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to messageboard!');
-  });
+
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {MessageboardComponent} from './components/messageboard/messageboard.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MessageComponent} from './components/message/message.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {MessageService} from './services/message/message.service';
+import {NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
+import {AlertComponent} from './components/alert/alert.component';
+import {CommentService} from './services/message/comment.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessageboardComponent,
+    MessageComponent,
+    AlertComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    NgbAlertModule
   ],
-  providers: [],
+  providers: [MessageService, CommentService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
